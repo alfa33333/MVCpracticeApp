cb69eb3f3100d986955ab0e21cde304555b279c7
Antonio Herrera Martin <44150484+alfa33333@users.noreply.github.com>
2025-03-27 17:22:20 -0600

    add the create controller and view

--- FinanceApp/Controllers/ExpensesController.cs
+++ FinanceApp/Controllers/ExpensesController.cs
@@ -1,4 +1,5 @@
 using FinanceApp.Data;
+using FinanceApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinanceApp.Controllers
@@ -18,6 +19,20 @@ namespace FinanceApp.Controllers
             var expenses = _context.Expenses.ToList();
 
             return View(expenses);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Expenses.Add(expense);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
         }
     }
 }
