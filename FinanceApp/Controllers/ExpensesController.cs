3020a903dec614fcb6611bac1f4efaa2b25ee24b
Antonio Herrera Martin <44150484+alfa33333@users.noreply.github.com>
2025-03-27 13:24:34 -0600

    add the expense controller file and its basic index page

---
+++ FinanceApp/Controllers/ExpensesController.cs
@@ -0,0 +1,21 @@
+using FinanceApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceApp.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        // GET: ExpensesController
+        private readonly FinanceAppContext _context;
+
+        public ExpensesController(FinanceAppContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
